d5cb1ae7665d9525ca322cc8a9282a47faccc96b
SergeyErshov <s.ershov.shoko@gmail.com>
2023-01-27 13:08:35 +0300

    add second node for elk

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,4 +12,17 @@ module "yandex_instance_1" {
   zone_name             = "ru-central1-a"
 }
 
+module "yandex_instance_2" {
+  source                = "./modules/create_vm"
+  disk_name             = "sys-elksrv"
+  disk_size             = "40"
+  disk_type             = "network-ssd"
+  cores                 = "2"
+  memory                = "4"
+  instance_family_image = "ubuntu-2204-lts"
+  instance_subnet_name  = "a-ru-central1-a"
+  instance_name         = "elksrv"
+  instance_desc         = "Elastick stack server"
+  zone_name             = "ru-central1-a"
+}
 
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -10,4 +10,14 @@ output "external_ipaddr_vm01" {
   value       = module.yandex_instance_1.external_ipaddr_vm
 }
 
+output "internal_ipaddr_vm02" {
+  description = "internal ip 0f VM"
+  value       = module.yandex_instance_2.internal_ipaddr_vm
+}
+
+output "external_ipaddr_vm02" {
+  description = "external ip of VM"
+  value       = module.yandex_instance_2.external_ipaddr_vm
+}
+
 
